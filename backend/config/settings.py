9984015407838cbf6fc8130d03dd04bc4f8c6421
LushinEA <lushin.ea@ispras.ru>
2025-07-02 18:39:44 +0300

    Site ver: 0.0.1

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -4,7 +4,52 @@ DATABASES = {
         'NAME': 'wpt_db',
         'USER': 'wpt_user',
         'PASSWORD': 'wpt_password',
-        'HOST': 'db',  # имя сервиса в docker-compose
-        'PORT': 5432,
+        'HOST': 'db',  # <--- важно!
+        'PORT': '5432',
     }
 }
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'users'
+    # + приложения
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],  # если хочешь, можешь здесь указать свои директории шаблонов
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',  # обязательно
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',  # обязательно
+    'django.contrib.messages.middleware.MessageMiddleware',     # обязательно
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+
+ALLOWED_HOSTS = ['*']
+
+ROOT_URLCONF = 'config.urls'
+
+AUTH_USER_MODEL = 'users.User'
