8a38e7ed4617098c84c3f612f8786302afa958a1
LushinEA <lushin.ea@ispras.ru>
2025-07-03 11:13:17 +0300

    Add auto create groups

--- backend/users/signals.py
+++ backend/users/signals.py
@@ -1,31 +1,35 @@
 from django.db.models.signals import post_migrate
+from django.db import transaction
 from django.contrib.auth.models import Group, Permission
 from django.dispatch import receiver
 from django.contrib.contenttypes.models import ContentType
+from users.models import User
 
 @receiver(post_migrate)
 def create_default_groups(sender, **kwargs):
-    # Пример для пользователей
-    admin_group, created = Group.objects.get_or_create(name='Администратор')
-    chief_group, created = Group.objects.get_or_create(name='Начальник автоколонны')
-    logistic_group, created = Group.objects.get_or_create(name='Специалист логистики')
-    accountant_group, created = Group.objects.get_or_create(name='Специалист учета')
+    def do_it():
+        user_content_type = ContentType.objects.get_for_model(User)
+        permissions = Permission.objects.filter(content_type=user_content_type)
 
-    # Зададим права
-    # Для примера дадим им все права на пользователей
-    from users.models import User
-    user_content_type = ContentType.objects.get_for_model(User)
-    permissions = Permission.objects.filter(content_type=user_content_type)
+        if not permissions.exists():
+            print("⚠ Permissions for User model still not created, skipping this cycle...")
+            return
 
-    # Админ — все права
-    admin_group.permissions.set(permissions)
+        # Создаём группы если их нет
+        admin_group, _ = Group.objects.get_or_create(name='Администратор')
+        chief_group, _ = Group.objects.get_or_create(name='Начальник автоколонны')
+        logistic_group, _ = Group.objects.get_or_create(name='Специалист логистики')
+        accountant_group, _ = Group.objects.get_or_create(name='Специалист учета')
 
-    # Начальник автоколонны — только просмотр пользователей
-    view_perm = Permission.objects.get(codename='view_user')
-    chief_group.permissions.set([view_perm])
+        # Админам все права
+        admin_group.permissions.set(permissions)
 
-    # Логистики — тоже только просмотр
-    logistic_group.permissions.set([view_perm])
+        try:
+            view_perm = Permission.objects.get(codename='view_user')
+            chief_group.permissions.set([view_perm])
+            logistic_group.permissions.set([view_perm])
+            accountant_group.permissions.set([view_perm])
+        except Permission.DoesNotExist:
+            print("⚠ view_user permission does not exist yet.")
 
-    # Учёт — тоже просмотр
-    accountant_group.permissions.set([view_perm])
+    transaction.on_commit(do_it)
